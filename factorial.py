4ea44003987544865716086524e38d26f19d3074
DHEERAJ KUMAR SAIN <124269483+810-prog@users.noreply.github.com>
2025-07-08 19:44:26 +0530

    Update factorial.py

---
+++ factorial.py
@@ -0,0 +1,15 @@
+# 4. Write a while loop to calculate the factorial of a given number.
+num = 1 
+mul = 1
+while num <=5:
+    mul = mul * num 
+    num +=1
+    print("the factorial given number",mul)
+
+
+# output 
+# the factorial given number 1
+# the factorial given number 2
+# the factorial given number 6
+# the factorial given number 24
+# the factorial given number 120
