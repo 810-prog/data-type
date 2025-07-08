f6c6ba882818c2023ac0b7f8a8758939f191c4f1
DHEERAJ KUMAR SAIN <124269483+810-prog@users.noreply.github.com>
2025-07-08 19:42:46 +0530

    Update vowelchecker.py

---
+++ vowelchecker.py
@@ -0,0 +1,17 @@
+#  Vowel or Consonant Checker
+#  Ask the user to input a single letter and determine if it's a vowel, consonant, or an invalid character.
+character = "ae"
+# match character:
+#     case "a":
+#         print("a")
+if character in "aeiouAEIOU":
+    print("its a vowel ")
+else:
+    print("its a consnent")
+
+
+# output
+# its a vowel
+
+
+
